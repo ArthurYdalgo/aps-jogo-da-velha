386f6afa5f527ee6970ce6721c5c525644d383a4
Arthur Ydalgo <arthurc@alunos.utfpr.edu.br>
2021-05-13 01:55:27 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,26 +1,22 @@
-#!/usr/bin/env python3
+import socket,pickle
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-import socket, pickle
-from time import sleep
+ip_serv = '127.0.0.1'
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-arr = [[-1,-1,1],[-1,1,-1],[1,-1,-1]]
-data_string = pickle.dumps(arr)
-a = 0
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        
-        s.connect((HOST, PORT))
-        print("enviando {}".format(a))
-        s.sendall(pickle.dumps(a))  
-        print("aguardando resposta")      
-        data = s.recv(1024)
-        print("recebido")
-        a+=1
-        # sleep(1)
+porta_serv = 65432
 
-    
-    
+dest = (ip_serv,porta_serv)
 
-print('Received', repr(data))
+
+
+tcp.connect(dest)
+msg = int(input("Digite um numero: "))
+
+tcp.send(pickle.dumps(msg))
+
+tamp_resp = int.from_bytes(tcp.recv(4),'big')
+resp = tcp.recv(tamp_resp)
+
+print(resp.decode())
+tcp.close()
+
